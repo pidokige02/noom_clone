09e1f64f8bfc492105bd609316732bd4ba30cbc3
pidokige02 <pidokige0204@gmail.com>
2022-02-01 15:43:29 +0900

    1.4 WebSocket Message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,15 +2,20 @@
 //window.location.host tells us where we are this can be applicable to web and modile phone
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-// socket.addEventListener("open", () =>{
-//     console.log("Connected to Server");
-// });
+// register eventlisterer function for "open", "message" and "close" event for the socket.
+socket.addEventListener("open", () =>{
+    console.log("Connected to Server");
+});
 
-// socket.addEventListener("message", (message) =>{
-//     console.log("Just got this:", message, "from ths server");
-// });
+socket.addEventListener("message", (message) =>{
+    console.log("New message: ", message.data);
+});
 
+socket.addEventListener("close", () =>{
+    console.log("disconnected from Server🤦‍♂️");
+});
 
-// socket.addEventListener("close", () =>{
-//     console.log("disconnected from Server🤦‍♂️");
-// });
+//register timer event triggered after 10 seconds
+setTimeout(() => {
+    socket.send("hello from the browser!");
+},10000)
