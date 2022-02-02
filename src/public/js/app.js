8a5753b6c360f80ba6b46016f8300baa3c3a8f79
pidokige02 <pidokige0204@gmail.com>
2022-02-02 23:33:49 +0900

    #2.0 SocketIO vs WebSockets

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,49 +1,75 @@
-// codes below are for frond end
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
+const socket = io();
 
-//window.location.host tells us where we are this can be applicable to web and modile phone
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-function makeMessage(type, payload){
-    const msg = {type, payload};    // turn data into object
-    return JSON.stringify(msg);     // turn object into string
-}
-
-// register eventlisterer function for "open", "message" and "close" event for the socket.
-socket.addEventListener("open", () =>{
-    console.log("Connected to Server");
-});
-
-socket.addEventListener("message", (message) =>{
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
-
-socket.addEventListener("close", () =>{
-    console.log("disconnected from Server🤦‍♂️");
-});
-
-//register timer event triggered after 10 seconds
-// setTimeout(() => {
-//     socket.send("hello from the browser!");
-// },10000)
-
-function handleSubmit(event){
+function handleRoomSubmit(event){
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
+    const input = form.querySelector("input");
+    socket.emit("room", {payload:input.value}, () => {
+        console.log("server is done");
+    }); // "room" is event ID, socket.io can send object directly.
     input.value = "";
 }
 
-function handleNickSubmit(event){
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname",input.value));
-    input.value = "";
-}
+form.addEventListener("submit", handleRoomSubmit);
 
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+
+
+
+
+
+
+
+
+
+
+// // codes below are for frond end using websocket
+// const messageList = document.querySelector("ul");
+// const nickForm = document.querySelector("#nick");
+// const messageForm = document.querySelector("#message");
+
+// //window.location.host tells us where we are this can be applicable to web and modile phone
+// const socket = new WebSocket(`ws://${window.location.host}`);
+
+// function makeMessage(type, payload){
+//     const msg = {type, payload};    // turn data into object
+//     return JSON.stringify(msg);     // turn object into string
+// }
+
+// // register eventlisterer function for "open", "message" and "close" event for the socket.
+// socket.addEventListener("open", () =>{
+//     console.log("Connected to Server");
+// });
+
+// socket.addEventListener("message", (message) =>{
+//     const li = document.createElement("li");
+//     li.innerText = message.data;
+//     messageList.append(li);
+// });
+
+// socket.addEventListener("close", () =>{
+//     console.log("disconnected from Server🤦‍♂️");
+// });
+
+// //register timer event triggered after 10 seconds
+// // setTimeout(() => {
+// //     socket.send("hello from the browser!");
+// // },10000)
+
+// function handleSubmit(event){
+//     event.preventDefault();
+//     const input = messageForm.querySelector("input");
+//     socket.send(makeMessage("new_message", input.value));
+//     input.value = "";
+// }
+
+// function handleNickSubmit(event){
+//     event.preventDefault();
+//     const input = nickForm.querySelector("input");
+//     socket.send(makeMessage("nickname",input.value));
+//     input.value = "";
+// }
+
+// messageForm.addEventListener("submit", handleSubmit);
+// nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http" // pre install package in node.js
-import WebSocket from "ws"
+import SocketIO from "socket.io";
 import express from "express"
 
 const app = express();
@@ -10,47 +10,77 @@ app.use("/public", express.static(__dirname + "/public"));  //expose only public
 app.get("/", (_,res) => res.render("home")); // rendering template "home" for the root route .
 app.get("/*", (_,res) => res.redirect("/")); // catch all other url
 
-const handleListen = () => console.log('Listening on http://localhost:3000')
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-// create http server from express application.
-const server = http.createServer(app);
-// create webSocket server on top of http server
-// if http server isn't required, you don't need to put "server" parameter to WebSocket.Server ({server})
-const wss = new WebSocket.Server ({server})
-
-function handleConnection(socket)   {
-    console.log(socket);
-}
-
-
-const sockets = [];
-// wss.on("connection", handleConnection);
-// below codes are executed for each browser connected to the server
-wss.on("connection",(socket) => {
-    sockets.push(socket);  // save socket whenever new socket is established for multiple browsers.
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser😂");
-    socket.on("close", () => {
-        console.log("disconnected from the Browser✔");
-    })
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach(aSocket =>
-                    aSocket.send(`${socket.nickname}: ${message.payload}`))
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-     });
+wsServer.on ("connection", (socket) =>{
+    socket.on("room",(msg, done) =>{
+        console.log(msg);
+        setTimeout(() => {
+            done();
+        },10000);
+    });
 });
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+// code for lesson0~1 teaching websocket
+// import http from "http" // pre install package in node.js
+// import WebSocket from "ws"
+// import express from "express"
+
+// const app = express();
+
+// app.set("view engine", "pug");
+// app.set("views", __dirname +"/views");
+// app.use("/public", express.static(__dirname + "/public"));  //expose only public folder to the user (security reason)
+// app.get("/", (_,res) => res.render("home")); // rendering template "home" for the root route .
+// app.get("/*", (_,res) => res.redirect("/")); // catch all other url
+
+// const handleListen = () => console.log('Listening on http://localhost:3000')
+
+// // create http server from express application.
+// const server = http.createServer(app);
+// // create webSocket server on top of http server
+// // if http server isn't required, you don't need to put "server" parameter to WebSocket.Server ({server})
+// const wss = new WebSocket.Server ({server})
+
+// function handleConnection(socket)   {
+//     console.log(socket);
+// }
+
+
+// const sockets = [];
+// // wss.on("connection", handleConnection);
+// // below codes are executed for each browser connected to the server
+// wss.on("connection",(socket) => {
+//     sockets.push(socket);  // save socket whenever new socket is established for multiple browsers.
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser😂");
+//     socket.on("close", () => {
+//         console.log("disconnected from the Browser✔");
+//     })
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type) {
+//             case "new_message":
+//                 sockets.forEach(aSocket =>
+//                     aSocket.send(`${socket.nickname}: ${message.payload}`))
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//         }
+//      });
+// });
+
+// server.listen(3000, handleListen);
 
 
 
 
-
+//////////////////////////////////////////////////////////////////////////////////////
 // sample code for lesson #0
 // import express from "express"
 
